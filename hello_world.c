82d02f61849ec9ae1ba3dfea97b5e496905d101f
Troy Bates <troy.b.bates@gmail.com>
2025-05-05 17:30:57 -0400

    updates to hello world moving pico

--- hello_world.c
+++ hello_world.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
-#include "pico/stdlib.h"
+// #include "pico/stdlib.h"
 
 int main()
 {
-    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    // const uint LED_PIN = PICO_DEFAULT_LED_PIN;
 
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
+    // gpio_init(LED_PIN);
+    // gpio_set_dir(LED_PIN, GPIO_OUT);
 
     stdio_init_all();
 
-    while (true)
+    while (1)
     {
         printf("Hello, from Raspberry Pi Pico!\n");
-        gpio_put(LED_PIN, 1);
-        sleep_ms(500);
-        gpio_put(LED_PIN, 0);
-        sleep_ms(500);
+        // gpio_put(LED_PIN, 1);
+        // sleep_ms(500);
+        // gpio_put(LED_PIN, 0);
+        // sleep_ms(500);
     }
 
     return 0;
